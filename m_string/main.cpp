4060e3e954308a87aaa6c80f5aafa305e9848688
silkRoad <garook>
2022-06-09 13:58:13 +0900

    덧셈연산

--- m_string/main.cpp
+++ m_string/main.cpp
@@ -19,6 +19,44 @@ public:
 		strcpy(_Myptr, str);
 	}
 
+	m_string& operator=(const m_string& rhs)
+	{
+		if(_Myptr != nullptr)
+			delete []_Myptr;
+
+		_Mysize = rhs._Mysize;
+		_Myptr = new char[_Mysize + 1];
+		strcpy(_Myptr, rhs._Myptr);
+
+		return *this;
+	}
+
+	m_string& operator=(const char* str)
+	{
+		delete []_Myptr;
+
+		_Mysize = strlen(str);
+		_Myptr = new char[_Mysize + 1];
+		strcpy(_Myptr, str);
+
+		return *this;
+	}
+
+	m_string operator+(const m_string& rhs)
+	{
+		m_string temp;
+		temp._Mysize = _Mysize + rhs._Mysize;
+		temp._Myptr = new char[temp._Mysize + 1];
+		
+		strcpy(temp._Myptr, _Myptr);
+		strcat(temp._Myptr, rhs._Myptr);
+				
+		delete []_Myptr;
+		_Myptr = nullptr;
+
+		return temp;
+	}
+
 	friend ostream& operator<<(ostream& os, const m_string& ms)
 	{
 		os << ms._Myptr;
@@ -37,7 +75,16 @@ private:
 int main(void)
 {
 	m_string str1 = "123";
+	cout << str1 << " " << str1.size() << endl;
+
 	m_string str2 = str1;
 	cout << str2 << " " << str2.size() << endl;
+
+	str2 = "m_string";
+	cout << str2 << " " << str2.size() << endl;
+
+	str2 = str2 + str1;
+	cout << str2 << " " << str2.size() << endl;
+
 	return 0;
 }
