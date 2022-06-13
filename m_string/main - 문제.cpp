704de31713d4d122f232a55d37200c1c6e8a8cdb
newJWP <garook@e-mirim.hs.kr>
2022-06-14 00:18:50 +0900

    문제 출제

---
+++ m_string/main - 문제.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class m_string {
+
+	friend ostream& operator<<(ostream& os, const m_string& ms)
+	{
+		os << ms._Myptr;
+		return os;
+	}
+};
+
+
+int main(void)
+{
+	m_string str1 = "123";
+	cout << str1 << " " << str1.size() << endl;
+
+	m_string str2 = str1;
+	cout << str2 << " " << str2.size() << endl;
+
+	str2 = "m_string";
+	cout << str2 << " " << str2.size() << endl;
+
+	str2 = str2 + str1;
+	cout << str2 << " " << str2.size() << endl;
+
+	str2 = str2 + "ab";
+	cout << str2 << " " << str2.size() << endl;
+
+	cout << (str1 == "123") << endl;
+	cout << (str1 == str2) << endl;
+
+	return 0;
+}
--- m_string/main.cpp
+++ m_string/main.cpp
@@ -93,7 +93,7 @@ public:
 private:
 	unsigned int _Mysize;
 	char* _Myptr;
-};
+}; 
 
 
 int main(void)
